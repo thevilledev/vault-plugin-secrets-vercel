6114138ec26add09b83fc77c18a6dce1866b3f22
Ville Vesilehto <ville@vesilehto.fi>
2023-07-02 08:54:36 +0300

    cleanup main

--- cmd/vault-plugin-secrets-vercel/main.go
+++ cmd/vault-plugin-secrets-vercel/main.go
@@ -11,23 +11,26 @@ import (
 
 func main() {
 	apiClientMeta := &api.PluginAPIClientMeta{}
-	logger := hclog.New(&hclog.LoggerOptions{})
 
 	flags := apiClientMeta.FlagSet()
 	if err := flags.Parse(os.Args[1:]); err != nil {
-		logger.Error("failed to parse CLI flags", "error", err)
-		os.Exit(1)
+		fatal(err)
 	}
 
 	tlsConfig := apiClientMeta.GetTLSConfig()
 	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
 
-	err := plugin.Serve(&plugin.ServeOpts{
+	if err := plugin.Serve(&plugin.ServeOpts{
 		BackendFactoryFunc: vercelPlugin.Factory,
 		TLSProviderFunc:    tlsProviderFunc,
-	})
-	if err != nil {
-		logger.Error("plugin shutting down", "error", err)
-		os.Exit(1)
+	}); err != nil {
+		fatal(err)
 	}
+}
+
+func fatal(err error) {
+	logger := hclog.New(&hclog.LoggerOptions{})
+	logger.Error("plugin shutting down", "error", err)
+	os.Exit(1)
+
 }
